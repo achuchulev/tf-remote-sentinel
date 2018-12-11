8a6559fd74c4a9f28c9bf501fd50224b367bb863
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-12-11 14:37:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,18 +8,18 @@ terraform {
   }
 }
 
-#provider "aws" {
-#  region = "us-east-2"
-#}
-
-#resource "aws_instance" "example" {
-#  ami           = "ami-0daad02ff4ba87139"
-#  instance_type = "t2.micro"
-#}
-
-resource "null_resource" "MultiHelloWorld" {
-  count = "2"
-  provisioner "local-exec" {
-    command = "echo Hello world ${count.index + 1}!"
-  }
+provider "aws" {
+  region = "us-east-2"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0daad02ff4ba87139"
+  instance_type = "t2.micro"
+}
+
+#resource "null_resource" "MultiHelloWorld" {
+#  count = "2"
+#  provisioner "local-exec" {
+#    command = "echo Hello world ${count.index + 1}!"
+#  }
+#}
