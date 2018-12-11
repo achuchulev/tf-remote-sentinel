3b120e000af13ecbff47b2588d68b9a3fb423508
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-12-11 14:20:28 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "remote" {
+    organization = "atanasc"
+
+    workspaces {
+      name = "tf-with-sentinel"
+    }
+  }
+}
+
+#provider "aws" {
+#  region = "us-east-2"
+#}
+
+#resource "aws_instance" "example" {
+#  ami           = "ami-0daad02ff4ba87139"
+#  instance_type = "t2.micro"
+#}
+
+resource "null_resource" "MultiHelloWorld" {
+  count = "2"
+  provisioner "local-exec" {
+    command = "echo Hello world ${count.index + 1}!"
+  }
+}
